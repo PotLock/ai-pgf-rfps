c1d8d348faee6b3e73886c02087afdf7cd60048b
kurodenjiro <kurodenjiro1@gmail.com>
2024-09-14 00:07:41 +0700

    fix markdown

--- components/rfp/add-rfp.tsx
+++ components/rfp/add-rfp.tsx
@@ -1,10 +1,12 @@
 'use client'
 
-import Markdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
+import { MemoizedReactMarkdown } from "@/components/markdown";
 import { useWalletSelector } from "@/app/contexts/WalletSelectorContext"
 import { utils } from "near-api-js";
 import { useEffect } from 'react';
+import { CodeBlock } from '@/components/ui/codeblock'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
 
 export const AddRFP = ({ props: { body, label } }: { props: any }) => {
     const { modal, accountId, selector } = useWalletSelector();
@@ -32,11 +34,49 @@ export const AddRFP = ({ props: { body, label } }: { props: any }) => {
         })
     }
     return (
-        <div className="p-4 text-green-400 border rounded-xl bg-zinc-950">
-            <div className="inline-block float-right px-2 py-1 text-xs rounded-full bg-white/10">
-                Draft
-            </div>
-            <Markdown remarkPlugins={[remarkGfm]}>{body}</Markdown>
+
+        <div className="flex-1 px-1 ml-4 space-y-2 overflow-hidden">
+            <MemoizedReactMarkdown
+                className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
+                remarkPlugins={[remarkGfm, remarkMath]}
+                components={{
+                    p({ children }) {
+                        return <p className="mb-2 last:mb-0">{children}</p>
+                    },
+                    code({ node, inline, className, children, ...props }) {
+                        if (children.length) {
+                            if (children[0] == '▍') {
+                                return (
+                                    <span className="mt-1 cursor-default animate-pulse">▍</span>
+                                )
+                            }
+
+                            children[0] = (children[0] as string).replace('`▍`', '▍')
+                        }
+
+                        const match = /language-(\w+)/.exec(className || '')
+
+                        if (inline) {
+                            return (
+                                <code className={className} {...props}>
+                                    {children}
+                                </code>
+                            )
+                        }
+
+                        return (
+                            <CodeBlock
+                                key={Math.random()}
+                                language={(match && match[1]) || ''}
+                                value={String(children).replace(/\n$/, '')}
+                                {...props}
+                            />
+                        )
+                    }
+                }}
+            >
+                {body}
+            </MemoizedReactMarkdown>
             {accountId ?
                 <button
                     onClick={publish}
@@ -46,9 +86,10 @@ export const AddRFP = ({ props: { body, label } }: { props: any }) => {
                 </button>
                 : <button
                     onClick={() => modal.show()}
-                    className="w-full px-4 py-2 mt-6 font-bold bg-green-400 rounded-lg text-zinc-900 hover:bg-green-500" >
+                    className="w-full px-4 py-2 mt-6 font-bold  bg-background transition-opacity rounded-lg text-zinc-900 hover:text-accent-foreground" >
                     Please Login to Publish
                 </button>}
         </div>
+
     )
 }
